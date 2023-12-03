c173fb51d159feaf565c1d8c3e748c237100592a
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-12-03 14:56:42 +0100

    Finished task 1 of day 2

---
+++ day2/task2.py
@@ -0,0 +1,71 @@
+r_max = 12
+g_max = 13
+b_max = 14
+
+def load_data():
+    with open('game_doc') as f:
+        data = []
+        for line in f.readlines():
+            data.append(line.strip())
+
+        data2 = []
+        for game in data:
+            game = game.replace(' ', '')
+            game = game.split(':')
+            game = game[1].split(';')
+            index = 0
+            for hand in game:
+                hand = hand.split(',')
+                game[index] = hand
+                index += 1
+            data2.append(game)
+
+        return data2
+
+
+def valid_color(color):
+    if 'red' in color:
+        color = color.replace('red', '')
+        if int(color) > r_max:
+            return False
+    elif 'green' in color:
+        color = color.replace('green', '')
+        if int(color) > g_max:
+            return False
+    elif 'blue' in color:
+        color = color.replace('blue', '')
+        if int(color) > b_max:
+            return False
+    return True
+
+
+def valid_hand(hand):
+    for color in hand:
+        if not valid_color(color):
+            return False
+    return True
+
+
+def valid_game(game):
+    for hand in game:
+        if not valid_hand(hand):
+            return False
+    return True
+
+
+def main():
+    data = load_data()
+
+    game_no = 1
+    sum = 0
+    for game in data:
+        if valid_game(game):
+            sum += game_no
+        game_no += 1
+
+    print(data)
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
